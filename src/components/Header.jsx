8cc630e04e90e76dbf206d07ac3384d9bfcc1694
Laurence Unabia <laurenceunabia@gmail.com>
2024-05-18 22:12:26 +0800

    tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 // import { styled } from "styled-components";
 
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 // import classes from './Header.module.css';
 
 // const StyledHeader = styled.header`
@@ -46,10 +46,16 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className=" object-contain mb-8 w-44 h-44"
+      />
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
